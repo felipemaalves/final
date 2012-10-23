4b96c92f307713e39b5b22328a9883923c14cea3
felipemaalves <felipemaalves@gmail.com>
2012-10-23 09:59:38 -0200

    base das Views + URLs

---
+++ sitedj/productspecs/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
+
+# uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(
+                queryset=Poll.objects.order_by('pub_date')[:5],
+                context_object_name='latest_poll_list',
+                template_name='polls/index.html'
+        )
+
+    ),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+                model=Poll,
+                template_name='polls/detail.html'
+        )
+    ),
+    url(r'^(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+                model=Poll,
+                template_name='polls/results.html'
+        ),
+        name='poll_results'
+    ),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'
+    ),
+)
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
