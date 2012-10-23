ec1deb57f84cc2b2f81086c36f81b98b93378ed3
felipemaalves <felipemaalves@gmail.com>
2012-10-23 17:40:05 -0200

    urls em andamento

--- sitedj/sitedj/urls.py
+++ sitedj/sitedj/urls.py
@@ -5,13 +5,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^productspecs/', include('productspecs.urls')),
     # Examples:
     # url(r'^$', 'sitedj.views.home', name='home'),
     # url(r'^sitedj/', include('sitedj.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
